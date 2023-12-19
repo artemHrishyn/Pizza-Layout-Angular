7339d4cdd829c837832ffd7bdaa8edc4f096e6ee
Artem Hrishyn <artem.hrishyn.it@gmail.com>
2023-12-19 22:08:16 +0200

    add header component

---
+++ src/app/components/main-block/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'piz-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
