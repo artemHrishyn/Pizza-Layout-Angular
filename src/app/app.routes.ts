d2967dfe5f9b7e6480a3af4c952803bc2850373b
Artem Hrishyn <artem.hrishyn.it@gmail.com>
2023-12-23 21:09:31 +0200

    add new pages menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
+import { MenuComponent } from './pages/menu/menu.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'main', pathMatch: 'full' },
     { path: "main", component: MainComponent },
-    { path: "menu", component: MainComponent },
+    { path: "menu", component: MenuComponent },
     { path: "order", component: MainComponent },
     { path: "about-as", component: MainComponent },
 ];
---
+++ src/app/pages/menu/menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'piz-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.scss'
+})
+export class MenuComponent {
+
+}
